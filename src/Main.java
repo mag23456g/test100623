1f3715760ec1b4fcb9b59888ebbba771a51c167a
Yuliia <angelama2408@gmail.com>
2024-02-02 21:00:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str1 = "Java";
+        String str2 = new String();
+        String str3 = new String(new char [] {'h','e', 'l', 'l', 'o'});
+        String str4 = new String(new char [] {'w','e', 'l', 'k', 'o', 'm', 'e'}, 3,4);
+
+
+
+        System.out.println(str1.length());
+    }
+}
